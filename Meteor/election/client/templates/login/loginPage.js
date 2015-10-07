6c822c4ec8af4b79c3cba8b4f880c3a1d4c3f828
Eyal Ben David <eyal.ben.david@sap.com>
2015-10-07 22:45:48 +0300

    add small change in the fb login

---
+++ Meteor/election/client/templates/login/loginPage.js
@@ -0,0 +1,17 @@
+Template._loginButtonsLoggedInDropdown.events({
+    'click #login-buttons-edit-profile': function(event) {
+        Router.go('profileEdit');
+    }
+});
+
+
+accountsUIBootstrap3.logoutCallback = function(error) {
+  if(error) console.log("Error:" + error);
+    if (Meteor.isClient) {
+      var _logout = Meteor.logout;
+      Meteor.logout = function customLogout() {
+        // Do your thing here
+        _logout.apply(Meteor, arguments);
+      }
+    }
+}
--- Meteor/election/server/accounts.js
+++ Meteor/election/server/accounts.js
@@ -1,6 +1,6 @@
 Accounts.onCreateUser(function (options, user) {
     if ((options || {}).profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=small";
         options.profile.email = user.services.facebook.email;
         options.profile.link = user.services.facebook.link;
         user.profile = options.profile;

57b1d222fe5a4eccc137111d9b72a53c00fd2a23
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-08 15:53:05 +0300

    fix css

--- Meteor/election/client/templates/left-menu/left-menu.js
+++ Meteor/election/client/templates/left-menu/left-menu.js
@@ -1,7 +1,13 @@
 Template.LeftMenu.events({
+
     'click a[name="donate"]': function (event) {
         Router.go('/donate');
+    },
+
+    'click img.img-circle': function (event) {
+        Router.go('/');
     }
+
 });
 
 Router.route('/donate', function () {

e022a61edff4a86943f545cd5ba31b5b49149616
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-08 15:49:34 +0300

    fix top header

--- Meteor/election/lib/router.js
+++ Meteor/election/lib/router.js
@@ -3,10 +3,7 @@ Router.configure({
     layoutTemplate: 'appBody',
 
     // the appNotFound template is used for unknown routes and missing lists
-    //notFoundTemplate: 'appNotFound',
-
-    // show the appLoading template whilst the subscriptions below load their data
-    //loadingTemplate: 'appLoading',
+    notFoundTemplate: 'appNotFound',
 
     progress : true,
 
@@ -27,10 +24,6 @@ if (Meteor.isClient) {
     // Keep showing the launch screen on mobile devices until we have loaded
     // the app's data
     dataReadyHold = LaunchScreen.hold();
-
-    // Show the loading screen on desktop
-    Router.onBeforeAction('loading', {except: ['join', 'signin']});
-    Router.onBeforeAction('dataNotFound', {except: ['join', 'signin']});
 }
 
 Router.route('/', function () {

433351f37b406fdba48b1fd166d5aa8a8f3f272e
Nati Ari <nati.ari@sap.com>
2015-10-11 10:08:40 +0300

    add angular start project

---
+++ Meteor/appolitics/public/angular/Angular_Seed_Project/js/articleController.js
@@ -0,0 +1,15 @@
+(function(){
+
+    /**
+     * MainCtrl - controller
+     */
+    function ArticleCtrl($scope, $http, $stateParams, $location) {
+        $http.get('/election/Meteor/appolitics/public/article.json?uuid=' + $stateParams.uuid).success(function(data) {
+            console.log('>>>>>',data);
+            $scope.content = data.content;
+        }.bind(this));
+
+    };
+    angular.module('inspinia').controller('ArticleCtrl', ArticleCtrl);
+
+}());

9f96a055ecff47c4717d06277252283cf87e735b
Nati Ari <nati.ari@sap.com>
2015-10-11 17:24:16 +0300

    add feedback

--- Meteor/appolitics/public/angular/Angular_Seed_Project/js/articleController.js
+++ Meteor/appolitics/public/angular/Angular_Seed_Project/js/articleController.js
@@ -5,9 +5,25 @@
      */
     function ArticleCtrl($scope, $http, $stateParams, $location) {
         $http.get('/election/Meteor/appolitics/public/article.json?uuid=' + $stateParams.uuid).success(function(data) {
-            console.log('>>>>>',data);
-            $scope.content = data.content;
+            $scope.article = data;
         }.bind(this));
+
+        $scope.feedBackPageNumber = 0;
+
+        $scope.loadFeedback = function loadFeedback () {
+            var response = $http.get('/election/Meteor/appolitics/public/user-feedback.json?uuid=' + $stateParams.uuid + '&page=' + $scope.feedBackPageNumber).success(function (data) {
+                $scope.userFeedbacks = $scope.userFeedbacks || [];
+                if (data && data.message && jQuery.isArray(data.message)) {
+                    $scope.userFeedbacks = $scope.userFeedbacks.concat(data.message);
+                }
+            }.bind(this));
+            console.log(response);
+        }
+
+        $scope.onloadMoreFeedback = function(){
+            $scope.loadFeedback();
+        }
+        $scope.loadFeedback();
 
     };
     angular.module('inspinia').controller('ArticleCtrl', ArticleCtrl);

6ed7ebfd52133fe10c9293ebad2e1221bb0fe45c
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-13 01:51:14 +0300

    simplify user profile

--- Meteor/election/client/templates/users/edit.js
+++ Meteor/election/client/templates/users/edit.js
@@ -6,23 +6,48 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.editUser.onRendered(function() {
+        //Meteor.defer(function () {
+        this.$('.datetimepicker').datetimepicker({
+            format: 'MMMM DD, YYYY'
+        });
+    });
+
     Template.editUser.helpers({
         email: function () {
             return getCurrentUser().profile.email;
         },
-        provider: function() {
+        provider: function () {
             return getCurrentUser().profile.provider;
         },
-        profile: function() {
+        profile: function () {
             return getCurrentUser().profile.link;
         },
-        lastLogin: function() {
+        lastLogin: function () {
             return getCurrentUser().profile.updatedAt;
         },
-
+        firstName: function () {
+            return getUserProfile().firstName || getCurrentUser().profile.name.split(' ')[0];
+        },
+        middleName: function () {
+            return getUserProfile().firstName;
+        },
+        lastName: function () {
+            return getUserProfile().firstName || getCurrentUser().profile.name.split(' ')[1];
+        },
+        birthday: function () {
+            return getUserProfile().firstName;
+        },
+        address: function () {
+            return getUserProfile().firstName;
+        }
     });
 
     function getCurrentUser() {
         return Accounts.users.findOne(Router.current().params.id) || {profile: {}};
+    }
+
+    function getUserProfile() {
+        return AccountProfile.findOne({user_id: getCurrentUser()._id}) || {};
     }
 }

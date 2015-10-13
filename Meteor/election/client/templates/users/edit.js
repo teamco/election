6a4edcc21ecef3c268e84ceb567e50ac58c61479
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-13 14:52:13 +0300

    fix code

--- Meteor/election/client/templates/users/edit.js
+++ Meteor/election/client/templates/users/edit.js
@@ -1,64 +1,65 @@
-if (Meteor.isClient) {
-
-    Template.editUser.events({
-        'click #editUserModal button[rel="submit"]': function (event, template) {
-            event.preventDefault();
-        }
-    });
-
-    Template.editUser.events({
-        'click #editUserModal button[rel="cancel"]': function (event, template) {
-            event.preventDefault();
-            Template.userData.__helpers.get('hideModal')();
-            Router.go('/users');
-        }
-    });
-
-    Template.editUser.onRendered(function() {
-        this.$('.datetimepicker').datetimepicker({
-            format: 'MMMM DD, YYYY'
-        });
-    });
-
-    Template.editUser.helpers({
-        email: function () {
-            return getCurrentUser().profile.email;
-        },
-        provider: function () {
-            return getCurrentUser().profile.provider;
-        },
-        profile: function () {
-            return getCurrentUser().profile.link;
-        },
-        lastLogin: function () {
-            return getCurrentUser().profile.updatedAt;
-        },
-        firstName: function () {
-            return getUserProfile().firstName || getUserName().split(' ')[0];
-        },
-        middleName: function () {
-            return getUserProfile().middleName;
-        },
-        lastName: function () {
-            return getUserProfile().lastName || getUserName().split(' ')[1];
-        },
-        birthday: function () {
-            return getUserProfile().birthday;
-        },
-        address: function () {
-            return getUserProfile().address;
-        }
-    });
-
-    function getUserName() {
-        return getCurrentUser().profile.name || ' ';
+Template.editUser.events({
+    'click #editUserModal button[rel="submit"]': function (event, template) {
+        closeEditProfile(event);
     }
+});
 
-    function getCurrentUser() {
-        return Accounts.users.findOne(Router.current().params.id) || {profile: {}};
+Template.editUser.events({
+    'click #editUserModal button[rel="cancel"]': function (event, template) {
+        closeEditProfile(event);
     }
+});
 
-    function getUserProfile() {
-        return AccountProfile.findOne({user_id: getCurrentUser()._id}) || {};
+Template.editUser.onRendered(function () {
+    this.$('.datetimepicker').datetimepicker({
+        format: 'MMMM DD, YYYY'
+    });
+});
+
+Template.editUser.helpers({
+    email: function () {
+        return getCurrentUser().profile.email;
+    },
+    provider: function () {
+        return getCurrentUser().profile.provider;
+    },
+    profile: function () {
+        return getCurrentUser().profile.link;
+    },
+    lastLogin: function () {
+        return getCurrentUser().profile.updatedAt;
+    },
+    firstName: function () {
+        return getUserProfile().firstName || getUserName().split(' ')[0];
+    },
+    middleName: function () {
+        return getUserProfile().middleName;
+    },
+    lastName: function () {
+        return getUserProfile().lastName || getUserName().split(' ')[1];
+    },
+    birthday: function () {
+        return getUserProfile().birthday;
+    },
+    address: function () {
+        return getUserProfile().address;
     }
+});
+
+function getUserName() {
+    return getCurrentUser().profile.name || ' ';
+}
+
+function getCurrentUser() {
+    return Accounts.users.findOne(Router.current().params.id) || {profile: {}};
+}
+
+function getUserProfile() {
+    return AccountProfile.findOne({user_id: getCurrentUser()._id}) || {};
+}
+
+function closeEditProfile(event) {
+    event.preventDefault();
+    Template.userData.__helpers.get('hideModal')();
+    Router.go('/users');
 }
--- Meteor/election/client/templates/users/users.js
+++ Meteor/election/client/templates/users/users.js
@@ -1,57 +1,54 @@
-if (Meteor.isClient) {
+Template.userData.events({
+    "click button[rel='editUser']": function (event, template) {
+        var nextUrl = '/users/' + this._id,
+            currentUrl = Router.current().originalUrl;
 
-    Template.userData.events({
-        "click button[rel='editUser']": function (event, template) {
-            var nextUrl = '/users/' + this._id,
-                currentUrl = Router.current().originalUrl;
-
-            if (currentUrl.indexOf(nextUrl) > -1) {
-                return template.view.template.__helpers.get('showModal')(this._id);
-            }
-
-            Router.go(nextUrl);
+        if (currentUrl.indexOf(nextUrl) > -1) {
+            return Template.userData.__helpers.get('showModal')(this._id);
         }
-    });
 
-    // This code only runs on the client
-    Template.userData.helpers({
-        showModal: function (id) {
-            if (id) {
-                Meteor.defer(function () {
-                    $('#editUserModal').modal();
-                });
-            }
-        },
-        hideModal: function () {
+        Router.go(nextUrl);
+    }
+});
+
+// This code only runs on the client
+Template.userData.helpers({
+    showModal: function (id) {
+        if (id) {
             Meteor.defer(function () {
-                $('#editUserModal').modal("hide");
+                $('#editUserModal').modal();
             });
-        },
-        allUsers: function () {
-            return Accounts.users.find().fetch();
-        },
-        isCurrentUser: isCurrentUser,
-        beforeRemove: function () {
-            return function (collection, _id) {
-                var user = collection.findOne(_id),
-                    name = user.profile.name || user.profile.email,
-                    scope = this;
-
-                BootstrapModalPrompt.prompt({
-                    title: "Delete user",
-                    content: 'Do you really want to remove user: ' + name + '?'
-                }, function (confirmed) {
-                    if (confirmed) {
-                        scope.remove();
-                        if (isCurrentUser(_id)) {
-                            Meteor.logout();
-                        }
-                    }
-                });
-            };
         }
-    });
-}
+    },
+    hideModal: function () {
+        Meteor.defer(function () {
+            $('#editUserModal').modal("hide");
+        });
+    },
+    allUsers: function () {
+        return Accounts.users.find().fetch();
+    },
+    isCurrentUser: isCurrentUser,
+    beforeRemove: function () {
+        return function (collection, _id) {
+            var user = collection.findOne(_id),
+                name = user.profile.name || user.profile.email,
+                scope = this;
+
+            BootstrapModalPrompt.prompt({
+                title: "Delete user",
+                content: 'Do you really want to remove user: ' + name + '?'
+            }, function (confirmed) {
+                if (confirmed) {
+                    scope.remove();
+                    if (isCurrentUser(_id)) {
+                        Meteor.logout();
+                    }
+                }
+            });
+        };
+    }
+});
 
 function isCurrentUser(_id) {
     return _id === Meteor.userId();

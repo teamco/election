b0f335cb72cb0922b6d0ca84f264b0324f73dcdf
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-23 09:03:55 +0300

    add confirm on delete user

--- appolitics/client/views/users/users.controller.ng.js
+++ appolitics/client/views/users/users.controller.ng.js
@@ -9,9 +9,17 @@ angular.module('appoliticsApp').controller('UsersCtrl', function ($scope) {
     };
 
     $scope.beforeDeleteUser = function (user) {
-
+        BootstrapModalPrompt.prompt({
+            title: "Confirm",
+            content: "Do you really want to delete user?"
+        }, function (result) {
+            if (result) {
+                // User confirmed it, so go do something.
+            } else {
+                // User did not confirm, do nothing.
+            }
+        });
     };
-
 
 
     $scope.allUsers = Accounts.users.find().fetch();

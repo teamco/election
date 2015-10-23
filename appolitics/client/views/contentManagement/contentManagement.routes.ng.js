540fd9f972e603aed720d9d22d27b1395320c4db
Vladimir Tkach <vladimir.tkach@sap.com>
2015-10-23 08:25:55 +0300

    fix namespace

--- appolitics/client/views/contentManagement/contentManagement.routes.ng.js
+++ appolitics/client/views/contentManagement/contentManagement.routes.ng.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 angular.module('appoliticsApp').config(function ($stateProvider) {
     $stateProvider.state('contentManagement', {
         url: '/contentManagement',
         templateUrl: 'client/views/contentManagement/contentManagement.view.ng.html',
-        controller: 'CntentManagementCtrl'
+        controller: 'ContentManagementCtrl'
     });
 });

6ff495d81b01304162e352396851c2d52590b189
Nati Ari <nati.ari@sap.com>
2015-10-26 23:28:38 +0200

    add end user project

---
+++ EndUser/client/config/router.js
@@ -0,0 +1,27 @@
+Router.configure({
+    layoutTemplate: 'mainLayout',
+    notFoundTemplate: 'notFound'
+
+});
+
+//
+// Example pages routes
+//
+
+
+Router.route('/candidates', function () {
+    this.render('candidates');
+});
+
+Router.route('/pageOne', function () {
+    this.render('pageOne');
+});
+
+Router.route('/pageTwo', function () {
+    this.render('pageTwo');
+});
+
+Router.route('/', function () {
+    Router.go('pageOne');
+});
+

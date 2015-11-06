30cd228c440f97c43b2b4bd372e709e43f7d2927
Vladimir Tkach <vladimir.tkach@sap.com>
2015-11-07 00:52:52 +0200

    add i18n

---
+++ appolitics/client/config/language.js
@@ -0,0 +1,20 @@
+getUserLanguage = function () {
+    // Put here the logic for determining the user language
+
+    return "en";
+};
+
+if (Meteor.isClient) {
+
+    Meteor.startup(function () {
+
+        Session.set("showLoadingIndicator", true);
+
+        TAPi18n.setLanguage(getUserLanguage()).done(function () {
+            Session.set("showLoadingIndicator", false);
+        }).fail(function (error_message) {
+            // Handle the situation
+            Bert.alert(error_message, 'danger');
+        });
+    });
+}
